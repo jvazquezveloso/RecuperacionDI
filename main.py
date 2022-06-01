d303fffd8000d6a1fbcc6a21609d22e98fd30c01
oracle <jvazquezveloso@danielcastelao.org>
2022-06-01 11:46:53 +0200

    Gtk Builder

--- main.py
+++ main.py
@@ -13,6 +13,19 @@ class Main(Gtk.Window):
         builder = Gtk.Builder()
         builder.add_from_file("recuperacion.glade")
 
+        window1 = builder.get_object("window1")
+        grid1 = builder.get_object("grid1")
+
+        lblInicio = builder.get_object("lblInicio")
+        lblNome = builder.get_object("lbNome")
+        lblContra = builder.get_object("lblContra")
+
+        entryNome = builder.get_object("entryNome")
+        entryContra = builder.get_object("entryContra")
+
+        btnLogin = builder.get_object("btnLogin")
+        comboLogin = builder.get_object("comboLogin")
+
         bd = conexionBase.ConexionBD("base.dat")
         bd.conectaBD()
         bd.creaCursor()
